03cf870886bfd8135cf99b701e5ae1b489fe525b
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-11-05 22:01:57 +0100

    chore: added Author card

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ const faqs = [
     title: "Do you ship to countries outside the EU?",
     text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
   },
+  {
+    title: "Created By:",
+    text: "Raphael Fadimu",
+  },
 ];
 
 export default function App() {
